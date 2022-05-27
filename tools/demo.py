10d5c2fa43b893f74ee978531f9da81c07ab6832
prakharrai2512 <prakharrai2512@gmail.com>
2022-05-27 12:21:05 +0530

    wtf

--- tools/demo.py
+++ tools/demo.py
@@ -50,13 +50,13 @@ def detect(cfg,opt):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model1 = get_net(cfg)
-    model = torch.jit.load(cfg)
+    model = get_net(cfg)
+    model1 = torch.jit.load("torchik.pt")
     checkpoint = torch.load(opt.weights, map_location= device)
     model.load_state_dict(checkpoint['state_dict'])
     model1.load_state_dict(checkpoint['state_dict'])
     model = model.to(device)
-    model1 = model.to(device)
+    model1 = model1.to(device)
     if half:
         model.half()  # to FP16
 
@@ -80,9 +80,9 @@ def detect(cfg,opt):
 
     vid_path, vid_writer = None, None
     img = torch.zeros((1, 3, opt.img_size, opt.img_size), device=device)  # init img
-    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
-    model.eval()
-
+    _ = model1(img.half() if half else img) if device.type != 'cpu' else None  # run once
+    model1.eval()
+    
     #m = torch.jit.trace(model, (img))
     #print(m)
     #torch.jit.save(m, 'combustken.pt')
@@ -97,8 +97,8 @@ def detect(cfg,opt):
             img = img.unsqueeze(0)
         # Inference
         t1 = time_synchronized()
-        da_seg_out,ll_seg_out= model(img)
-        det_out = model1.detecthead(img)
+        da_seg_out,ll_seg_out= model1(img)
+        det_out = model.detecthead(img)
         t2 = time_synchronized()
         # if i == 0:
         #     print(det_out)
